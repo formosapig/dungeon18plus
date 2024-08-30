931f61da9d63967c3a60eaf77e1b2242cbe2592c
gohome@home <gohomexx@gmail.com>
2024-08-30 21:57:58 +0800

    簡單的實作 iOS 版的 url scheme

--- ios/src/com/qqhouse/dungeon18plus/IOSLauncher.java
+++ ios/src/com/qqhouse/dungeon18plus/IOSLauncher.java
@@ -1,16 +1,26 @@
 package com.qqhouse.dungeon18plus;
 
-import org.robovm.apple.foundation.NSAutoreleasePool;
-import org.robovm.apple.uikit.UIApplication;
-
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.backends.iosrobovm.IOSApplication;
 import com.badlogic.gdx.backends.iosrobovm.IOSApplicationConfiguration;
-import com.qqhouse.dungeon18plus.Main;
+
+import org.robovm.apple.foundation.NSAutoreleasePool;
+import org.robovm.apple.foundation.NSURL;
+import org.robovm.apple.uikit.UIApplication;
+import org.robovm.apple.uikit.UIApplicationOpenURLOptions;
 
 public class IOSLauncher extends IOSApplication.Delegate {
+
+    @Override
+    public boolean openURL(UIApplication app, NSURL url, UIApplicationOpenURLOptions options) {
+        Gdx.app.error("IOSLauncher", "openURL : " + url);
+        return false;
+    }
+
     @Override
     protected IOSApplication createApplication() {
         IOSApplicationConfiguration config = new IOSApplicationConfiguration();
+
         //config.statusBarVisible = true;
         return new IOSApplication(new Main(), config);
     }
